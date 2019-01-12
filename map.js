7954c84a1eebe301d60df2c7e4d8d77d6901eaa2
mbeader <matt.beader@gmail.com>
2019-01-11 22:10:17 -0500

    refactor

--- map.js
+++ map.js
@@ -9,48 +9,14 @@ socket.on('veh', function (veh) {
   context.clearRect(0, 0, canvas.width, canvas.height);
   if(typeof track !== "undefined") {
     context.lineWidth = 15;
-    context.strokeStyle = 'navy';
-    context.beginPath();
-    for(let i = 0; i < track.s1.length; i++)
-      context.lineTo(track.s1[i].x+500, 500-track.s1[i].y);
-    context.lineTo(track.s2[0].x+500, 500-track.s2[0].y);
-    context.stroke();
-    context.strokeStyle = 'green';
-    context.beginPath();
-    for(let i = 0; i < track.s2.length; i++)
-      context.lineTo(track.s2[i].x+500, 500-track.s2[i].y);
-    context.lineTo(track.s3[0].x+500, 500-track.s3[0].y);
-    context.stroke();
-    context.strokeStyle = 'maroon';
-    context.beginPath();
-    for(let i = 0; i < track.s3.length; i++)
-      context.lineTo(track.s3[i].x+500, 500-track.s3[i].y);
-    context.lineTo(track.s1[0].x+500, 500-track.s1[0].y);
-    context.stroke();
+    drawSector(track.s1, track.s2[0], 'navy');
+    drawSector(track.s2, track.s3[0], 'green');
+    drawSector(track.s3, track.s1[0], 'maroon');
     
     context.fillStyle = 'gray';
-    let angle;
-    angle = -1*Math.atan2(track.s1[1].y - track.s3[track.s3.length-1].y, track.s1[1].x - track.s3[track.s3.length-1].x);
-    context.save();
-    context.translate(track.s1[0].x+500, 500-track.s1[0].y);
-    context.rotate(angle);
-    context.translate((track.s1[0].x+500)*-1, (500-track.s1[0].y)*-1);
-    context.fillRect(track.s1[0].x+500-3, 500-track.s1[0].y-15, 6, 30);
-    context.restore();
-    angle = -1*Math.atan2(track.s2[1].y - track.s1[track.s1.length-1].y, track.s2[1].x - track.s1[track.s1.length-1].x);
-    context.save();
-    context.translate(track.s2[0].x+500, 500-track.s2[0].y);
-    context.rotate(angle);
-    context.translate((track.s2[0].x+500)*-1, (500-track.s2[0].y)*-1);
-    context.fillRect(track.s2[0].x+500-3, 500-track.s2[0].y-15, 6, 30);
-    context.restore();
-    angle = -1*Math.atan2(track.s3[1].y - track.s2[track.s2.length-1].y, track.s3[1].x - track.s2[track.s2.length-1].x);
-    context.save();
-    context.translate(track.s3[0].x+500, 500-track.s3[0].y);
-    context.rotate(angle);
-    context.translate((track.s3[0].x+500)*-1, (500-track.s3[0].y)*-1);
-    context.fillRect(track.s3[0].x+500-3, 500-track.s3[0].y-15, 6, 30);
-    context.restore();
+    drawSectorMarker(track.s3[track.s3.length-1], track.s1[0], track.s1[1]);
+    drawSectorMarker(track.s1[track.s1.length-1], track.s2[0], track.s2[1]);
+    drawSectorMarker(track.s2[track.s2.length-1], track.s3[0], track.s3[1]);
   }
 
   let fillcolor, textcolor;
@@ -70,14 +36,12 @@ socket.on('veh', function (veh) {
       classcolors[veh[num].c].text = textcolor;
     }
     context.strokeStyle = 'black';
-    //context.fillStyle = 'white';
     context.fillStyle = fillcolor;
     context.lineWidth = 2;
     context.beginPath();
     context.arc(veh[num].x+500, 500-veh[num].y, 10, 0, Math.PI * 2, true);
     context.stroke();
     context.fill();
-    //context.fillStyle = 'black';
     context.fillStyle = textcolor;
     context.font = '12px serif';
     context.textAlign = 'center';
@@ -86,7 +50,6 @@ socket.on('veh', function (veh) {
 });
 
 socket.on('map', function (map) {
-  console.log('got');
   track = map;
 });
 
@@ -101,4 +64,23 @@ document.addEventListener('DOMContentLoaded', initLoad, false);
 function initLoad(e) {
   canvas = document.getElementById('map');
   context = canvas.getContext('2d');
+}
+
+function drawSector(sector, end, color) {
+  context.strokeStyle = color;
+    context.beginPath();
+    for(let i = 0; i < sector.length; i++)
+      context.lineTo(sector[i].x+500, 500-sector[i].y);
+    context.lineTo(end.x+500, 500-end.y);
+    context.stroke();
+}
+
+function drawSectorMarker(prev, center, next) {
+  let angle = -1*Math.atan2(next.y - prev.y, next.x - prev.x);
+  context.save();
+  context.translate(center.x+500, 500-center.y);
+  context.rotate(angle);
+  context.translate(-1*(center.x+500), -1*(500-center.y));
+  context.fillRect(center.x+500-3, 500-center.y-15, 6, 30);
+  context.restore();
 }
